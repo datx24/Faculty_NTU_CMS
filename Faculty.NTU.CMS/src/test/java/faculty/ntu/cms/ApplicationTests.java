8b6a41140eadef9527d97a29e9adb12312d562a2
datx24 <xuandat475@gmail.com>
2025-04-22 12:01:50 +0700

    tạo dự án falcutu ntu cms

---
+++ Faculty.NTU.CMS/src/test/java/faculty/ntu/cms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package faculty.ntu.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
