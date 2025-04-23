fec851d0eb71427b61e5d4e8d584e25cd9b39adb
datx24 <xuandat475@gmail.com>
2025-04-23 09:50:17 +0700

    iouu
    hugyu

---
+++ Faculty.NTU.CMS/src/main/java/faculty/ntu/cms/controllers/DashboardController.java
@@ -0,0 +1,13 @@
+package faculty.ntu.cms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+	
+	@GetMapping("/dashboard")
+	public String getDashBoard() {
+		return "pages/admin/dashboard";
+	}
+}
---
+++ Faculty.NTU.CMS/src/main/java/faculty/ntu/cms/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package faculty.ntu.cms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String getHome() {
+		return "pages/user/home";
+	}
+}
