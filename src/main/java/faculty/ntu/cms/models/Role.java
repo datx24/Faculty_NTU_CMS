e3059b413c9f48b127fb183670d2225ca59e4697
datx24 <xuandat475@gmail.com>
2025-04-23 21:51:30 +0700

    Cài đặt entity role

---
+++ src/main/java/faculty/ntu/cms/models/Role.java
@@ -0,0 +1,84 @@
+package faculty.ntu.cms.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String decription;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private List<User> users;
+
+    // Constructor
+    public Role() {
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
+    }
+
+    public Role(String name, String decription, List<User> users) {
+        this.name = name;
+        this.decription = decription;
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
+        this.users = users;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
