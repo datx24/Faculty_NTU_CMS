e792efac8ab544985c976b876115bfe6f7177620
datx24 <xuandat475@gmail.com>
2025-04-25 00:03:22 +0700

    cài userser service

---
+++ src/main/java/faculty/ntu/cms/services/UserService.java
@@ -0,0 +1,28 @@
+package faculty.ntu.cms.services;
+
+import faculty.ntu.cms.models.User;
+import faculty.ntu.cms.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return user;
+    }
+
+    //phương thức đăng kí
+    public void registerUser(User user) {
+        userRepository.save(user);
+    }
+}
