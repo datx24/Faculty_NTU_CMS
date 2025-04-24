c2c96e37f5d33898b569d73fe741e379d3d5e4f4
datx24 <xuandat475@gmail.com>
2025-04-25 00:03:51 +0700

    truyền tham số name sau khi đăng nhập thành công

--- src/main/java/faculty/ntu/cms/controllers/DashboardController.java
+++ src/main/java/faculty/ntu/cms/controllers/DashboardController.java
@@ -1,13 +1,23 @@
 package faculty.ntu.cms.controllers;
 
+import faculty.ntu.cms.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 	
 	@GetMapping("/dashboard")
-	public String getDashBoard() {
+	public String getDashBoard(Model m) {
+		String name = "";
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof User) { // Ép kiểu trực tiếp về User
+			name = ((User) principal).getName();
+		}
+		m.addAttribute("name", name);
+
 		return "pages/admin/dashboard";
 	}
 }
