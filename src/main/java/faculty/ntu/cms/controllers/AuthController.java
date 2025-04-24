f969ac2712bb7c87f30a2227f7aa51c4a5395ee2
datx24 <xuandat475@gmail.com>
2025-04-25 00:01:47 +0700

    cài auth controller

---
+++ src/main/java/faculty/ntu/cms/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package faculty.ntu.cms.controllers;
+
+import faculty.ntu.cms.models.User;
+import faculty.ntu.cms.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public String login() {
+        return "pages/admin/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model m) {
+        m.addAttribute("user", new User());
+        return "pages/admin/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
+}
