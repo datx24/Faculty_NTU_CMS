e9c8f14b5233ad617716e992b8e6bc4076f41cbb
datx24 <xuandat475@gmail.com>
2025-04-25 00:03:05 +0700

    cài UserRepository
    code findByUsername

---
+++ src/main/java/faculty/ntu/cms/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package faculty.ntu.cms.repositories;
+
+import faculty.ntu.cms.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
