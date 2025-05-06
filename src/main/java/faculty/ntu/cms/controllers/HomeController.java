5c75bed1971e728f3fb5b85767d5ba769dec14a6
datx24 <xuandat475@gmail.com>
2025-05-06 23:15:43 +0700

    Cài đặt HomeController
    truyền event đang hoạt động ra giao diện user

--- src/main/java/faculty/ntu/cms/controllers/HomeController.java
+++ src/main/java/faculty/ntu/cms/controllers/HomeController.java
@@ -1,12 +1,23 @@
 package faculty.ntu.cms.controllers;
 
+import faculty.ntu.cms.models.Event;
+import faculty.ntu.cms.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private EventService eventService;
+
 	@GetMapping("/")
-	public String getHome() {
+	public String getHome(Model m) {
+		List<Event> events = eventService.getAllActiveEvents();
+		m.addAttribute("events", events);
 		return "pages/user/home";
 	}
 }
