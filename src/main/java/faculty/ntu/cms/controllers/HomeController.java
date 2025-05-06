3a44c16e4526865813dde51fb745e11da84936f7
datx24 <xuandat475@gmail.com>
2025-05-07 01:45:08 +0700

    Cập nhật HomeController
    thêm biến notification service để truyền vào view home danh sách thông báo

--- src/main/java/faculty/ntu/cms/controllers/HomeController.java
+++ src/main/java/faculty/ntu/cms/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package faculty.ntu.cms.controllers;
 
 import faculty.ntu.cms.models.Event;
+import faculty.ntu.cms.models.Notification;
 import faculty.ntu.cms.services.EventService;
+import faculty.ntu.cms.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +15,15 @@ import java.util.List;
 public class HomeController {
 	@Autowired
 	private EventService eventService;
+	@Autowired
+	private NotificationService notificationService;
 
 	@GetMapping("/")
 	public String getHome(Model m) {
 		List<Event> events = eventService.getAllActiveEvents();
+		List<Notification> notifications = notificationService.getAllNotifications();
 		m.addAttribute("events", events);
+		m.addAttribute("notifications", notifications);
 		return "pages/user/home";
 	}
 }
