f6b921e75e9b8aa8c3735ee288e3fbdf4c5f96bb
datx24 <xuandat475@gmail.com>
2025-05-07 21:37:34 +0700

    Cài đặt home controller
    truyền biến menuitems để hiển thị cho user xem

--- src/main/java/faculty/ntu/cms/controllers/HomeController.java
+++ src/main/java/faculty/ntu/cms/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package faculty.ntu.cms.controllers;
 
 import faculty.ntu.cms.models.Event;
+import faculty.ntu.cms.models.MenuItem;
 import faculty.ntu.cms.models.Notification;
 import faculty.ntu.cms.services.EventService;
+import faculty.ntu.cms.services.MenuItemService;
 import faculty.ntu.cms.services.NotificationService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,13 +20,16 @@ public class HomeController {
 	private EventService eventService;
 	@Autowired
 	private NotificationService notificationService;
+	@Autowired
+	private MenuItemService menuItemService;
 
 	@GetMapping("/")
-	public String getHome(Model m) {
-		List<Event> events = eventService.getAllActiveEvents();
-		List<Notification> notifications = notificationService.getAllNotifications();
-		m.addAttribute("events", events);
-		m.addAttribute("notifications", notifications);
+	public String getHome(Model m, HttpServletRequest request) {
+		String requestURI = request != null ? request.getRequestURI() : "/";
+		m.addAttribute("currentPath", requestURI);
+		m.addAttribute("events", eventService.getAllActiveEvents());
+		m.addAttribute("notifications", notificationService.getAllNotifications());
+		m.addAttribute("menuItems", menuItemService.getActiveMenuItemsByMenuName("primary"));
 		return "pages/user/home";
 	}
 }
