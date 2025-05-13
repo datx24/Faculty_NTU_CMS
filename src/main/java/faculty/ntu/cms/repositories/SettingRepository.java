ff90ecacde1bd8eedc5bfa82749592765529839a
datx24 <xuandat475@gmail.com>
2025-05-13 17:18:41 +0700

    cập nhật SettingRepository
    findBySettingKey -> findByKey

--- src/main/java/faculty/ntu/cms/repositories/SettingRepository.java
+++ src/main/java/faculty/ntu/cms/repositories/SettingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SettingRepository extends JpaRepository<Setting, Integer> {
-    Optional<Setting> findBySettingKey(String settingKey);
+    Optional<Setting> findByKey(String settingKey);
 }
