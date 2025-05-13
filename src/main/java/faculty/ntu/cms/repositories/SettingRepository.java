48947c4ed8e03725ad61724b12a1c174f88df320
datx24 <xuandat475@gmail.com>
2025-05-13 10:29:23 +0700

    cài đặt SettingRepository

---
+++ src/main/java/faculty/ntu/cms/repositories/SettingRepository.java
@@ -0,0 +1,10 @@
+package faculty.ntu.cms.repositories;
+
+import faculty.ntu.cms.models.Setting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SettingRepository extends JpaRepository<Setting, Integer> {
+    Optional<Setting> findBySettingKey(String settingKey);
+}
