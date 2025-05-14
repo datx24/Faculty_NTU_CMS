fbb46c8e31fef3a194f372c4a52f232306019b8f
bobby-stanoff <hongnguyen210180@gmail.com>
2025-05-14 01:46:01 -0400

    update db migration

---
+++ src/main/resources/dbmigrations/tschema_update_14_may.sql
@@ -0,0 +1,9 @@
+INSERT INTO settings (id, setting_key, setting_value, description, created_by) VALUES
+(1, 'site_department', 'Tên khoa 2', 'Updated via settings form', 1),
+(3, 'site_university', 'Đại học Nha Trang', 'Updated via settings form', 1),
+(6, 'site_logo', '/uploads/1747156198979_quiz.png', 'Updated via settings form', 1),
+(7, 'site_banner', '/uploads/1747157098138_pexels-eberhardgross-691668...', 'banner trang home', 1),
+(8, 'about_content', 'Trường TOP 1 VIỆT NAM', 'Nội dung về cơ sở đào tạo', 1),
+(9, 'site_address', 'Địa chỉ cơ sở đào tạo', 'Địa chỉ cơ sở đào tạo', 1),
+(10, 'site_email', 'Email cơ sở đào tạo', 'Email cơ sở đào tạo', 1),
+(11, 'site_hotline', 'Số hotline cơ sở đào tạo', 'Số hotline cơ sở đào tạo', 1);
